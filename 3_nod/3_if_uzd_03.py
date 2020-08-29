a91f3709e85177c935fe98ac9af94d364e61143c
kristapspriede <kristaps.priede@gmail.com>
2020-08-29 18:22:31 +0300

    Created 3_if_uzd_03.py

---
+++ 3_nod/3_if_uzd_03.py
@@ -0,0 +1,37 @@
+"""
+
+Klients pieejot pie kases norāda, vai ir pieejama klienta karte.
+
+Klients ievadīs 4 preces.
+
+Klientu karte, katrai precei dod 11% atlaidi, bet pirmajai precei 12% atlaidi
+
+Klients ievada preces nosaukumu un cenu
+
+Programma izvada pilnā teikumā preces nosaukumu un cenu, ja ir pieejama klientu karte izvada cenu ar atlaidi
+
+Visas cenas ir jāsaskaita un beigās jāizvada
+
+Kopējā atlaižu summa arī ir jāsaskaita un jāizvada
+
+"""
+
+#irKarte =  1 # True = 1, False = 0
+
+irKarte = bool(irKarte) # pārvērš ievadīto 1 vai 0 par True vai False
+
+for i in range(4):
+
+    nosaukums = "nosaukums"
+
+    cena = 2.5
+
+    # pārbauda vai ir atlaižu karte
+
+        # izvada cenu ar/bez atlaides un pieskaita to kopējai summai
+
+    #summa += cena #
+
+    #summa_atlaide += summa*0.11
+
+    #summa_atlaide += summa*0.12   
