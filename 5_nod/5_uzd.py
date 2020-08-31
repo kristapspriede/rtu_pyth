d0a97fc87726f6171aff97e675f53d2ab3f3968d
kristapspriede <kristaps.priede@gmail.com>
2020-08-31 20:58:51 +0300

    Updated 5_uzd

--- 5_nod/5_uzd.py
+++ 5_nod/5_uzd.py
@@ -1,34 +1,34 @@
-# txt = "hidroelektrostacija"
-# # 1
-# print(txt[0])
-# # 2
-# print(txt[-1])
-# # 3
-# print(txt[::2])
-# # 4
-# print(txt[1:4])
-# # 5
-# vowel_counts = {}
-# for vowel in "aeiou":
-#     count = txt.count(vowel)
-#     vowel_counts[vowel] = count
-# counts = vowel_counts.values()
-# print(sum(counts))
-# # 6
-# print(txt.upper())
-# # 7
-# print(txt.lower())
-# # 8
-# print(txt.isalpha())
-# # 9
-# print(txt.replace("o", "0"))
-# # 10
-# if txt.isalpha():
-#     print(txt.isalpha())
-# else:
-#     print(txt.isalnum())
-# # 11
-# print(txt.find("k"))
+txt = "hidroelektrostacija"
+# 1
+print(txt[0])
+# 2
+print(txt[-1])
+# 3
+print(txt[::2])
+# 4
+print(txt[1:4])
+# 5
+vowel_counts = {}
+for vowel in "aeiou":
+    count = txt.count(vowel)
+    vowel_counts[vowel] = count
+counts = vowel_counts.values()
+print(sum(counts))
+# 6
+print(txt.upper())
+# 7
+print(txt.lower())
+# 8
+print(txt.isalpha())
+# 9
+print(txt.replace("o", "0"))
+# 10
+if txt.isalpha():
+    print(txt.isalpha())
+else:
+    print(txt.isalnum())
+# 11
+print(txt.find("k"))
 
 # 2. uzd
 # 1
